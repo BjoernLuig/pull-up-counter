12a18f8613e1a73c746924f6613a39ebe8debaa3
Bjoern Luig <bjoernluigqgmail.com>
2023-03-26 22:51:52 +0200

    fix counterdistance, screen, ...

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # pull-up-counter
 
+A device to count pull ups per day, week and month using an ultrasonic ditance sensor.
+// The counts are saved on the EEPROM.
+// The currunt time is optained through a time server at every boot.
+
 https://cad.onshape.com/documents/7eee93e0a96726aa8824ecba/w/20d27a70971ff588a796e390/e/e279968a8b694a0ca3ac5cfa
--- pull-up-counter.ino
+++ pull-up-counter.ino
@@ -6,34 +6,48 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <NewPing.h>
-#include <EEPROM.h>
 #include <WiFi.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include "secrets.h"
+#include <time.h>
+#include <Preferences.h>
+#include "secrets.h" // file where SSID and PASSWORD for your local network are defined
+
+// daily routine settings
+#define PULL_UP_GOAL 10 // your daily goal which keeps the display on until you achive it
+#define NEXT_DAY_HOUR 4 // when the daily counter should reset for the next da (optimal at 04:00)
+#define WAKE_UP_HOUR 8  // when the display can show your daily goal in the morning (08:00)
 
 // distance settings in cm
 #define MAX_DISTANCE 80     // maximum distance to detect anything (max 250)
-#define PULL_UP_DISTANCE 20 // distance under which a pull up is detected
+#define PULL_UP_DISTANCE 35 // distance under which a pull up is detected
 
-// timing settings in milliseconds
-#define LOOP_DELAY 100            // delay for main loop in milliseconds (optimal 100)
-#define DATA_SAVE_INTERVALL 60000 // save intervall of data to EEPROM (100.000 value changes per lifetime)
-#define RESET_TIMEOUT 10000       // reset data on EEPROM if in PULL_UP_DISTANCE for this time
-#define DISPLAY_TIMEOUT 30000     // timeout for display (only if the daily goal is achived)
+// datetime settings
+#define TIME_ZONE "CET-1CEST,M3.5.0/02,M10.5.0/03" // from https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+#define TIME_SERVER "pool.ntp.org"                 // address of timeserver
+struct tm timeInfo;
+char timeString[50];
+int curruntHour;
+int curruntWeekDay;
+int curruntMonth;
+int lastHour;
+int lastWeekDay;
+int lastMonth;
 
-// daily routine settings
-#define MIN_PULL_UPS_PER_DAY 10 // your daily goal which keeps the display on until you achive it
-#define MAX_WAKE_UP_HOUR 8      // when the display kann show your daily goal in the morning
-
-// display
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+// loop timing settings in milliseconds
+#define LOOP_DELAY 100               // delay for main loop in milliseconds (optimal 100)
+#define PULL_UP_DELAY 1000           // min time between to pull ups to count (optimal 1000)
+#define DATA_SAVE_INTERVALL 10000    // save intervall of data to EEPROM
+#define RESET_THIS_DAY_TIMEOUT 10000 // manuel reset day counter if in PULL_UP_DISTANCE for this time
+#define RESET_ALL_TIMEOUT 20000      // manuel reset all data on EEPROM if in PULL_UP_DISTANCE for this time
+#define DISPLAY_TIMEOUT 10000        // timeout for display (only if the daily goal is achived)
 
 // distance sensor
 #define TRIGGER_PIN 27
 #define ECHO_PIN 14
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 int distance;
+
+// display
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 // pull up counter
 boolean pullUpDetected = false;
@@ -46,16 +60,8 @@ long lastDetection = 0;
 long lastPullUpDetection = 0;
 long lastDataSave = 0;
 
-// datetime
-#define TIME_OFFSET 3600 // time offset to standard time (in german: winter 3600 second = 1 hour, summer 7200 = 2 hours)
-#define TIME_SERVER "pool.ntp.org"
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, TIME_SERVER);
-char timeString[50];
-int curruntWeekDay;
-int curruntMonth;
-int lastWeekDay;
-int lastMonth;
+// preferences
+Preferences preferences;
 
 void setup()
 {
@@ -73,137 +79,103 @@ void setup()
   display.println("setup");
   display.display();
 
-  // EEPROM
-  EEPROM.begin(6); // 3 2 byte integers
-  EEPROM.get(0, countThisDay);
-  EEPROM.get(2, countThisWeek);
-  EEPROM.get(4, countThisMonth);
-  EEPROM.get(6, curruntWeekDay);
-  EEPROM.get(8, curruntMonth);
+  // preferences
+  preferences.begin("data", false);
+  curruntHour = preferences.getInt("curruntHour", 0);
+  curruntWeekDay = preferences.getInt("curruntWeekDay", 0);
+  curruntMonth = preferences.getInt("curruntMonth", 0);
+  countThisDay = preferences.getInt("countThisDay", 0);
+  countThisWeek = preferences.getInt("countThisWeek", 0);
+  countThisMonth = preferences.getInt("countThisMonth", 0);
+  preferences.end();
 
   // connect wifi
   WiFi.begin(SSID, PASSWORD);
   Serial.print("connecting to wifi");
   display.print("connecting to wifi");
   display.display();
-  for (int i = 0; i < 60; i++)
-  { // try for 60 seconds
-    if (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print(".");
-      display.print(".");
-      display.display();
-      delay(1000);
-    }
-    else
-    {
-      break;
-    }
-  }
-  if (WiFi.status() != WL_CONNECTED)
+  while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println();
-    Serial.println("no wifi after 60 seconds");
-    Serial.println("please restart");
-    display.println();
-    display.println("no wifi after 60 seconds");
-    display.println("please restart");
+    Serial.print(".");
+    display.print(".");
     display.display();
-    while (true)
-      ;
-  }
-  else
-  {
-    Serial.println();
-    Serial.println("wifi connected");
-    display.println();
-    display.println("wifi connected");
-    display.display();
-
-    // connect to npt server
-    timeClient.begin();
-    timeClient.setTimeOffset(3600);
-    Serial.print("waiting for ntp");
-    display.print("waiting for ntp");
-    display.display();
-    while (!timeClient.update())
-    {
-      timeClient.forceUpdate();
-      Serial.print(".");
-      display.print(".");
-      display.display();
-      delay(500);
-    }
-    Serial.println("");
-    Serial.println("ntp time synced");
-    display.println("");
-    display.println("ntp time synced");
-    display.display();
-
-    // sync npt time with internal rtc
-    struct tm timeinfo;
-    time_t now = timeClient.getEpochTime();
-    gmtime_r(&now, &timeinfo);
-    struct timeval tv = {.tv_sec = mktime(&timeinfo)};
-    settimeofday(&tv, NULL);
-    Serial.print("new rtc time: ");
-    Serial.println(timeClient.getFormattedTime());
-    display.print("new rtc time: ");
-    display.println(timeClient.getFormattedTime());
-    display.display();
-
-    // disconnect wifi
-    WiFi.disconnect(true);
-    Serial.println("wifi disconnected");
-    display.println("wifi disconnected");
-    display.display();
+    delay(1000);
   }
   Serial.println();
+  Serial.println("wifi connected");
+  display.println();
+  display.println("wifi connected");
+  display.display();
+  delay(2000); // wail for readability
 
-  // delay to read display
-  delay(3000);
+  // connect to npt server and set timezone
+  configTime(0, 0, TIME_SERVER); // 0, 0 because we will use TZ in the next line
+  setenv("TZ", TIME_ZONE, 1);    // Set environment variable with your time zone
+  tzset();
+  while (!getLocalTime(&timeInfo))
+  {
+    Serial.println("failed to get time info");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("failed to get time info");
+    display.display();
+    delay(500);
+    display.clearDisplay();
+    display.display();
+    delay(500);
+  }
+  strftime(timeString, sizeof(timeString), "%d.%m %H:%M", &timeInfo);
+  Serial.print("new time: ");
+  Serial.println(timeString);
+  display.print("new time: ");
+  display.println(timeString);
+  display.display();
+
+  // disconnect wifi
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  Serial.println("wifi disconnected");
+  display.println("wifi disconnected");
+  display.display();
+  delay(2000); // wail for readability
 }
 
 void loop()
 {
 
   // datetime
-  struct tm timeInfo;
-  if (!getLocalTime(&timeInfo))
+  while (!getLocalTime(&timeInfo))
   {
-    Serial.println("failed to get time");
-    Serial.println("please restart");
+    Serial.println("failed to get time info");
     display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
     display.setCursor(0, 0);
-    display.println("failed to get time");
-    display.println("please restart");
+    display.println("failed to get time info");
     display.display();
-    return;
+    delay(500);
+    display.clearDisplay();
+    display.display();
+    delay(500);
   }
   strftime(timeString, sizeof(timeString), "%d.%m %H:%M", &timeInfo);
-  Serial.print("time: ");
-  Serial.println(timeString);
+  lastHour = curruntHour;
   lastWeekDay = curruntWeekDay;
   lastMonth = curruntMonth;
-  curruntWeekDay = timeInfo.tm_wday; // sunday = 0
+  curruntHour = timeInfo.tm_hour;
+  curruntWeekDay = timeInfo.tm_wday; // sunday = 0, monday =1, ...
   curruntMonth = timeInfo.tm_mon;    // januar = 0
-  EEPROM.put(6, curruntWeekDay);
-  EEPROM.put(8, curruntMonth);
 
   // reset counts on new day, week or month
-  if (curruntWeekDay != lastWeekDay)
+  if (curruntHour == NEXT_DAY_HOUR && curruntHour != lastHour)
   {
     countThisDay = 0;
     Serial.println("new day");
   }
-  if (curruntWeekDay == 1 && lastWeekDay == 0)
-  { // monday after sunday
+  if (curruntWeekDay == 1 && curruntWeekDay != lastWeekDay)
+  {
     countThisWeek = 0;
     Serial.println("new week");
   }
-  if (curruntMonth != lastMonth)
+  if (lastMonth != curruntMonth)
   {
     countThisMonth = 0;
     Serial.println("new month");
@@ -211,7 +183,7 @@ void loop()
 
   // detect distance
   distance = sonar.ping_cm();
-  if (distance != 0 && distance < MAX_DISTANCE)
+  if (distance != 0)
   {
     lastDetection = millis();
     Serial.print("detected distance: ");
@@ -228,68 +200,95 @@ void loop()
       lastPullUpDetection = millis();
       countThisDay++;
       countThisWeek++;
-      Serial.println("pull up detected");
+      countThisMonth++;
+      Serial.print("detected pull up at ");
+      Serial.println(timeString);
+      Serial.print("countThisDay: ");
+      Serial.println(countThisDay);
+      Serial.print("countThisWeek: ");
+      Serial.println(countThisWeek);
+      Serial.print("countThisMonth: ");
+      Serial.println(countThisMonth);
     }
   }
   else
   {
-    pullUpDetected = false;
+    if (millis() - lastPullUpDetection > PULL_UP_DELAY)
+    {
+      pullUpDetected = false;
+    }
   }
 
-  // reset counts on active reset
-  if (pullUpDetected && millis() - lastPullUpDetection > RESET_TIMEOUT)
+  // reset countsThisDay on active reset
+  if (pullUpDetected && millis() - lastPullUpDetection > RESET_THIS_DAY_TIMEOUT)
+  {
+    countThisWeek -= countThisDay;
+    countThisMonth -= countThisDay;
+    countThisDay = 0;
+    Serial.println("reset countThisDay on active reset");
+  }
+
+  // reset all counts on active reset
+  if (pullUpDetected && millis() - lastPullUpDetection > RESET_ALL_TIMEOUT)
   {
     countThisDay = 0;
     countThisWeek = 0;
     countThisMonth = 0;
-    Serial.println("reset counts on active reset");
+    Serial.println("reset all counts on active reset");
   }
 
-  // save counts to EEPROM
+  // save counts and time infos using preferences
   if (millis() - lastDataSave > DATA_SAVE_INTERVALL)
   {
     lastDataSave = millis();
-    EEPROM.put(0, countThisDay);
-    EEPROM.put(2, countThisWeek);
-    EEPROM.put(4, countThisMonth);
-    Serial.println("saved counts to EEPROM");
+    preferences.begin("data", false);
+    preferences.putInt("curruntHour", curruntHour);
+    preferences.putInt("curruntWeekDay", curruntWeekDay);
+    preferences.putInt("curruntMonth", curruntMonth);
+    preferences.putInt("countThisDay", countThisDay);
+    preferences.putInt("countThisWeek", countThisWeek);
+    preferences.putInt("countThisMonth", countThisMonth);
+    preferences.end();
+    Serial.print("saved counts and time infos");
   }
 
-  // serial
-  Serial.print("distance: ");
-  Serial.println(distance);
-  Serial.print("countThisDay: ");
-  Serial.println(countThisDay);
-  Serial.print("countThisWeek: ");
-  Serial.println(countThisWeek);
-  Serial.print("countThisMonth: ");
-  Serial.println(countThisMonth);
-  Serial.println();
-
   // display
+  bool timeout = millis() - lastDetection > DISPLAY_TIMEOUT;
+  bool earlyMorning = timeInfo.tm_hour <= WAKE_UP_HOUR && timeInfo.tm_hour >= NEXT_DAY_HOUR;
+  bool pullUpGoal = countThisDay >= PULL_UP_GOAL;
   display.clearDisplay();
-  if ((millis() - lastDetection < DISPLAY_TIMEOUT) || (countThisDay < MIN_PULL_UPS_PER_DAY && timeInfo.tm_hour >= MAX_WAKE_UP_HOUR))
+  if (!(timeout && (pullUpGoal || earlyMorning)))
   {
+    if (!pullUpGoal)
+    {
+      display.invertDisplay(true);
+    }
+    else
+    {
+      display.invertDisplay(false);
+    }
     display.setTextSize(1);
-    display.setCursor(30, 0);
+    display.setCursor(32, 4);
     display.println(timeString);
-    display.setTextSize(2);
-    display.setCursor(0, 16);
+    display.setTextSize(3);
+    display.setCursor(48, 17);
     display.print(countThisDay);
-    display.setCursor(24, 16);
-    display.println("pull ups");
     display.setTextSize(1);
-    display.setCursor(0, 40);
-    display.print("this week: ");
-    display.println(countThisWeek);
-    display.print("this month: ");
-    display.println(countThisMonth);
+    display.print(" /");
+    display.print(PULL_UP_GOAL);
+    display.setCursor(4, 42);
+    display.print("Week: ");
+    display.print(countThisWeek);
+    display.setCursor(64, 42);
+    display.print("Month: ");
+    display.print(countThisMonth);
+    display.setCursor(4, 52);
     display.print("distance: ");
     display.print(distance);
     display.print(" cm");
-    display.display();
   }
+  display.display();
 
-  // loop delay
+  // loop delay and empty Serial.println()
   delay(LOOP_DELAY);
 }
